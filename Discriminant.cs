2a35f48c40f6b76172601659b00d4a9610482cf3
Kami-969 <64987182+Kami-969@users.noreply.github.com>
2020-06-12 13:04:32 +0400

    Add files via upload

---
+++ Discriminant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Models
+{
+    public class Discriminant
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+
+        public int answer;
+
+        private void discriminant(int a, int b, int c)
+        {
+            double square_side = Convert.ToInt64(Math.Pow(b, 2) - 4 * -a * -c);
+
+
+            if (square_side > 0)
+            {
+                answer = 2;
+                
+            }
+            else if (square_side == 0)
+            {
+
+                answer = 1;
+            }
+            else
+            {
+                answer = 0;
+
+            }
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;   
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult print(int a,int b, int c)
+        {
+            var Disc = new Discriminant() { a = a, b = b, c = c };
+
+            ViewBag.gang = "gang";
+
+            if(Disc.answer == 2)
+            {
+                ViewBag.Warning= "am gantolebas aqvs 2 amonaxsni";
+            }
+            else if(Disc.answer == 1)
+            {
+                ViewBag.Warning = "am gantolebas aqvs 1 amonaxsni";
+            }
+            else if(Disc.answer == 0)
+            {
+                ViewBag.Warning = "am gantolebas aqvs 0 amonaxsni";
+            }
+
+            return View(Disc);
+        }
+
+        
+    }
+   
+  
+}
